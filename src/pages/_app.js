fc90c893660ba2123e3ba3cf9ebae40239decc95
Nicholas Gingerella <nicholas.gingerella@gmail.com>
2025-01-14 19:09:22 -0800

    dark theme toggle

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen dark bg-background">
-      <Component {...pageProps} />
-    </div>
-  );
+    <ThemeProvider>
+      <div className="min-h-screen bg-background">
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import JapaneseQuiz from '@/components/JapaneseQuiz'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4 min-h-screen">
+    <main className="container mx-auto p-4 min-h-screen relative">
+      <ThemeToggle />
       <JapaneseQuiz />
     </main>
   )
